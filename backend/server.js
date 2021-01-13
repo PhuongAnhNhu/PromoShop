409a1b9cb55aa5d805cfbf6fc04fb3f35ca614a0
Phương Anh Như <nhuphuonganh001@gmail.com>
2021-01-13 14:15:22 +0100

    es modules in node.js

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
-const express = require('express')
-const products = require('./data/products')
+import express from "express"
+import dotenv from "dotenv"
+import products from "./data/products.js"
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
-}) 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-}) 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-}) 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
 
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`));
