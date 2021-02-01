3eabcde50b5f76bc5f16a741a5c8facb77d28705
Phương Anh Như <nhuphuonganh001@gmail.com>
2021-02-01 11:06:20 +0100

    added config file

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
-import express from "express"
-import dotenv from "dotenv"
-import products from "./data/products.js"
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+import products from "./data/products.js";
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -16,5 +20,8 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
