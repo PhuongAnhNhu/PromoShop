0d2ff09a7de944446acad37d10099a1050538ec3
Phuong Anh Nhu <phuong.nhu@endava.com>
2021-02-01 11:39:43 +0100

    adding Colors to the Console

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-
+import colors from 'colors'
 
 const connectDB = async () => {
     try {
@@ -9,9 +9,9 @@ const connectDB = async () => {
             useCreateIndex: true
         })
         
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
     } catch (error) {
-        console.error(`Error: ${error.message}`)
+        console.error(`Error: ${error.message}`.red.underline.bold)
         process.exit(1)
     }
 }
