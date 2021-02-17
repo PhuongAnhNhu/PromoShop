ae14ebf237dd4066233b8e214668bd1bf19ce58f
Phuong Anh Nhu <phuong.nhu@endava.com>
2021-02-17 11:58:14 +0100

    Bring Redux State into HomeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,37 +1,37 @@
-import React, { Component } from "react";
-import { Col, Row } from "react-bootstrap";
-import Product from "../components/Product.js";
-import axios from "axios";
+import React, { useEffect } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import Product from '../components/Product.js';
+import { useDispatch, useSelector } from 'react-redux';
 
-export class HomeScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      products: [],
-    };
-  }
-  componentDidMount() {
-    //componentDidMount Methode wird ausgeführt nachdem die Komponenten in das DOM gerendert wurde.
-    const fetchProducts = async () => {
-      const { data } = await axios.get("api/products");
-      this.setState({products: data});
-    };
-    fetchProducts();
-  }
-  render() {
+import { listProducts } from '../actions/productActions';
+
+const HomeScreen = () => {
+    const dispatch = useDispatch();
+
+    const productList = useSelector(state => state.productList);
+    const { loading, error, products } = productList;
+    useEffect(() => {
+        dispatch(listProducts());
+    }, [dispatch]);
+
     return (
-      <div>
-        <h1>Lastest products</h1>
-        <Row>
-          {this.state.products.map((item) => (
-            <Col sm={12} md={6} key={item._id}>
-              <Product product={item} />
-            </Col>
-          ))}
-        </Row>
-      </div>
+        <div>
+            <h1>Lastest products</h1>
+            {loading ? (
+                <h2>Loading...</h2>
+            ) : error ? (
+                <h3>{error}</h3>
+            ) : (
+                <Row>
+                    {products.map(item => (
+                        <Col sm={12} md={6} key={item._id}>
+                            <Product product={item} />
+                        </Col>
+                    ))}
+                </Row>
+            )}
+        </div>
     );
-  }
-}
+};
 
 export default HomeScreen;
