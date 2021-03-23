25efc9a131cd7bd50c3172cfc3d41757840498b9
Anna Phuong Anh <phuong.nhu@endava.com>
2021-03-23 11:40:45 +0100

    Custom Authentication Middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,13 @@
 import express from 'express';
 const router = express.Router();
 
-import {
-    authUser
-} from '../controllers/userController.js';
+import { authUser, getUserProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddeware.js';
 
 //@desc Fetch all Users
 //@route GET /api/Users
 //@acess Public
-router.post('/login', authUser)
-
+router.post('/login', authUser);
+router.get('/profile', protect, getUserProfile);
 
 export default router;
