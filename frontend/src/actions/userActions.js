3fcc9decb2cfc1e04ce74186a2da1a341452d3d1
Anna Phuong Anh <phuong.nhu@endava.com>
2021-03-25 11:39:54 +0100

    Logout

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,9 +3,10 @@ import {
     USER_LOGIN_FAIL,
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
+    USER_LOGOUT,
 } from '../constants/userConstants';
 
-export const login = (email, password) => async (dispatch) => {
+export const login = (email, password) => async dispatch => {
     try {
         dispatch({
             type: USER_LOGIN_REQUEST,
@@ -17,7 +18,9 @@ export const login = (email, password) => async (dispatch) => {
             },
         };
         const { data } = await axios.post(
-            '/api/users/login', { email, password }, config
+            '/api/users/login',
+            { email, password },
+            config
         );
 
         dispatch({
@@ -34,4 +37,11 @@ export const login = (email, password) => async (dispatch) => {
                     : error.message,
         });
     }
+};
+
+export const logout = () => dispatch => {
+    localStorage.removeItem('userInfo');
+    dispatch({
+        type: USER_LOGOUT,
+    });
 };
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import { logout } from '../actions/userActions';
 
 const Header = () => {
+    const dispatch = useDispatch();
     const userLogin = useSelector(state => state.userLogin);
+
     const { userInfo } = userLogin;
+    
     const logoutHandler = () => {
-      console.log('logout');
+        dispatch(logout());
     };
     return (
         <header>
@@ -22,7 +26,7 @@ const Header = () => {
                         <Nav className="ml-auto">
                             <LinkContainer to="/cart">
                                 <Nav.Link>
-                                    <i className="fas fa-shopping-cart"></i>{' '}
+                                    <i className="fas fa-shopping-cart"></i>
                                     Cart
                                 </Nav.Link>
                             </LinkContainer>
