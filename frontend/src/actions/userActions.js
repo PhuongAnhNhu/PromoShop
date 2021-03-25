0019483915a7b10415f1019b109ec1908607a852
Anna Phuong Anh <phuong.nhu@endava.com>
2021-03-25 08:38:59 +0100

    User Login Screen and Functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -5,7 +5,7 @@ import {
     USER_LOGIN_SUCCESS,
 } from '../constants/userConstants';
 
-export const login = (email, password) => async dispatch => {
+export const login = (email, password) => async (dispatch) => {
     try {
         dispatch({
             type: USER_LOGIN_REQUEST,
@@ -17,7 +17,7 @@ export const login = (email, password) => async dispatch => {
             },
         };
         const { data } = await axios.post(
-            ('/api/users/login', { email, password }, config)
+            '/api/users/login', { email, password }, config
         );
 
         dispatch({
@@ -25,7 +25,7 @@ export const login = (email, password) => async dispatch => {
             payload: data,
         });
         localStorage.setItem('userInfo', JSON.stringify(data));
-    } catch (err) {
+    } catch (error) {
         dispatch({
             type: USER_LOGIN_FAIL,
             payload:
