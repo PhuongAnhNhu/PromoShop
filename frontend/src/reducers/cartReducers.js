2fc89a63d573c7783a93fcdf84298ec4b817c3b0
Anna Phuong Anh <phuong.nhu@endava.com>
2021-03-30 11:10:01 +0200

    Payment Screen and Sace Payment Method

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,10 +1,14 @@
 import {
     CART_ADD_ITEM,
     CART_REMOVE_ITEM,
+    CART_SAVE_PAYMENT_METHOD,
     CART_SAVE_SHPPING_ADDRESS,
 } from '../constants/cartConstants';
 
-export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, action) => {
+export const cartReducer = (
+    state = { cartItems: [], shippingAddress: {} },
+    action
+) => {
     switch (action.type) {
         case CART_ADD_ITEM:
             const item = action.payload;
@@ -34,8 +38,12 @@ export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, acti
         case CART_SAVE_SHPPING_ADDRESS:
             return {
                 ...state,
-                shippingAddress: action.payload
-                ,
+                shippingAddress: action.payload,
+            };
+        case CART_SAVE_PAYMENT_METHOD:
+            return {
+                ...state,
+                paymentMethod: action.payload,
             };
         default:
             return state;
