b4ec9b48bf45fb2881d6d33a6b61a7bb8f2e658d
Anna Phuong Anh <phuong.nhu@endava.com>
2021-04-07 13:12:55 +0200

    Update To Paid Endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 const router = express.Router();
 
-import { addOrdersItems, getOrderById } from '../controllers/orderController.js';
+import { addOrdersItems, getOrderById, updateOrderToPaid } from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddeware.js';
 
 router.post('/', protect, addOrdersItems);
 router.route('/:id').get(protect, getOrderById)
+router.route('/:id/paid').put(protect, updateOrderToPaid)
 
 export default router;
